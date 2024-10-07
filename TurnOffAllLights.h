da69d72d197c0d6a199d00c302e040ed3d7bac6b
Samkelekile Ndevu <81513440+samkelekile-ndevu@users.noreply.github.com>
2024-10-07 21:05:16 +0200

    Update TurnOffAllLights.h

--- TurnOffAllLights.h
+++ TurnOffAllLights.h
@@ -1,3 +1,15 @@
+/**
+ * @class TurnOffAllLights
+ * @brief Command class to turn off all light devices in the smart home automation system.
+ *
+ * This class implements the Command interface to provide the functionality 
+ * for deactivating all light devices within the system. It iterates through 
+ * the list of devices and invokes the toggle off action on each light device.
+ *
+ * @section methods Methods
+ * - execute(): Executes the command to turn off all light devices.
+ */
+
 #ifndef TURNOFFALLLIGHTS_H
 #define TURNOFFALLLIGHTS_H
 #include"Command.h"

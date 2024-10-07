2d0226b30871f72e57a91e9fd336e71b978baef9
Samkelekile Ndevu <81513440+samkelekile-ndevu@users.noreply.github.com>
2024-10-07 21:00:03 +0200

    Update LockAllDoors.h and added doxygen code

--- LockAllDoors.h
+++ LockAllDoors.h
@@ -1,3 +1,15 @@
+/**
+ * @class LockAllDoors
+ * @brief Command class to lock all door lock devices in the smart home automation system.
+ *
+ * This class implements the Command interface to provide the functionality 
+ * for locking all door lock devices within the system. It iterates through 
+ * the list of devices and invokes the lock action on each door lock device.
+ *
+ * @section methods Methods
+ * - execute(): Executes the command to lock all door lock devices.
+ */
+
 #ifndef LOCKALLDOORS_H
 #define LOCKALLDOORS_H
 #include"Command.h"

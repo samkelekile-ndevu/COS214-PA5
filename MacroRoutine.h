fecf803708b5eb1083680df5c93e3620957325ba
Samkelekile Ndevu <81513440+samkelekile-ndevu@users.noreply.github.com>
2024-10-07 21:00:54 +0200

    Update MacroRoutine.h
    Added doxygen code

--- MacroRoutine.h
+++ MacroRoutine.h
@@ -1,4 +1,18 @@
-//COMPOSTIE OF COMMANDS SO YOU CAN ALSO ADD COMMANDS WHEN NEEDED
+/**
+ * @class MacroRoutine
+ * @brief Represents a composite command that can execute a sequence of commands.
+ *
+ * The MacroRoutine class inherits from Command and allows for the 
+ * management of multiple Command objects. It can add or remove commands 
+ * from its list and execute them in sequence, enabling complex actions 
+ * to be performed as a single command.
+ *
+ * @section methods Methods
+ * - addProcedure(Command* command): Adds a command to the macro routine.
+ * - removeProcedure(Command* command): Removes a command from the macro routine.
+ * - execute(): Executes all commands in the order they were added.
+ */
+
 #ifndef MACROROUTINE_H
 #define MACROROUTINE_H
 #include"Command.h"

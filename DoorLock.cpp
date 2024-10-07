175e9d870b1672a3bb4a1a52a80b85ab4c95cf8a
chuck776 <uminathimdluli@gmail.com>
2024-10-08 01:11:13 +0200

    better logic for  DoorLock.cpp

--- DoorLock.cpp
+++ DoorLock.cpp
@@ -1,4 +1,5 @@
 #include"DoorLock.h"
+using namespace std;
 DoorLock::DoorLock()
 {
     isLocked = false;
@@ -10,8 +11,11 @@ string DoorLock::getStatus()const
 }
 void DoorLock::performAction(const string& action)
 {
-     if (action == "Lock") {isLocked = true;std::cout << "Door is now " << getStatus() << std::endl;}
-        else if (action == "Unlock") {isLocked = false;std::cout << "Door is now " << getStatus() << std::endl;}
+     if (action == "Lock") {isLocked = true;std::cout << "Door has been " << getStatus() << std::endl;}
+        else if (action == "Unlock") 
+        {isLocked = false;
+         std::cout << "Door has been " << getStatus() << std::endl;
+        }
         
 }
 string DoorLock::getDeviceType()const
@@ -22,6 +26,6 @@ void DoorLock::update(const string& sensorEvent)
 {
     if(sensorEvent == "motion detected")
     {
-        cout <<"Motion detected involving door.\n";
+        cout <<"Motion detected with the door sensors.\n";
     }
 }

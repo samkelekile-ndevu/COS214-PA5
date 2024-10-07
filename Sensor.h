a05800632c934e4a94b49bc9c5288a5ce43a11f9
Samkelekile Ndevu <81513440+samkelekile-ndevu@users.noreply.github.com>
2024-10-07 21:03:08 +0200

    Update Sensor.h

--- Sensor.h
+++ Sensor.h
@@ -1,3 +1,20 @@
+/**
+ * @class Sensor
+ * @brief Represents a sensor that detects events and notifies registered smart devices.
+ *
+ * The Sensor class allows for the addition and removal of smart devices to be notified
+ * when an event occurs. It can trigger notifications to all registered devices in response
+ * to detected events.
+ *
+ * @section methods Methods
+ * - addDevice(SmartDevice* device): Adds a smart device to the sensor's notification list.
+ *   - @param device Pointer to the SmartDevice to add.
+ * - removeDevice(SmartDevice* device): Removes a smart device from the sensor's notification list.
+ *   - @param device Pointer to the SmartDevice to remove.
+ * - notifyDevices(const std::string& event): Notifies all registered devices about a specific event.
+ *   - @param event A string representing the event that has been detected.
+ */
+
 #ifndef SENSOR_H
 #define SENSOR_H
 #include"SmartDevice.h"

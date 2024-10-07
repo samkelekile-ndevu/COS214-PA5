18c09c4ed6e119a90e4f944833a8263bac4ec122
Samkelekile Ndevu <81513440+samkelekile-ndevu@users.noreply.github.com>
2024-10-07 21:02:09 +0200

    Update Room.h

--- Room.h
+++ Room.h
@@ -1,3 +1,27 @@
+/**
+ * @class Room
+ * @brief Represents a room that can contain multiple smart devices.
+ *
+ * The Room class inherits from SmartDevice and serves as a composite that can 
+ * manage multiple smart devices. It allows adding and removing devices and 
+ * performing actions on all contained devices.
+ *
+ * @section methods Methods
+ * - Room(const string& rName): Constructor that initializes the room with a name.
+ * - addDevice(SmartDevice* addDevice): Adds a smart device to the room.
+ *   - @param addDevice Pointer to the SmartDevice to add.
+ * - removeDevice(SmartDevice* device): Removes a smart device from the room.
+ *   - @param device Pointer to the SmartDevice to remove.
+ * - getStatus(): Returns the current status of all devices in the room.
+ *   - @return A string representing the aggregated status of devices.
+ * - performAction(const string& action): Performs the specified action on all devices in the room.
+ *   - @param action A string representing the action to perform.
+ * - getDeviceType(): Returns the type of the device as "Room".
+ *   - @return A string representing the device type.
+ * - update(const string& sensor): Updates the status of devices in response to a sensor event.
+ *   - @param sensor A string representing the sensor event.
+ */
+
 #ifndef ROOM_H
 #define ROOM_H
 #include"SmartDevice.h"

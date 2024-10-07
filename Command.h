d657b6c6defa7ed8aa84ed4d6c3757dc4755d39d
Samkelekile Ndevu <81513440+samkelekile-ndevu@users.noreply.github.com>
2024-10-07 20:57:23 +0200

    Update Command.h
    Added doxygen code

--- Command.h
+++ Command.h
@@ -1,3 +1,15 @@
+/**
+ * @class Command
+ * @brief Abstract base class for command objects in the smart home automation system.
+ *
+ * This class defines the interface for command execution, allowing different commands
+ * to be implemented and executed in a uniform manner. It serves as a base class for 
+ * specific command implementations, such as locking doors or turning off lights.
+ *
+ * @section methods Methods
+ * - execute(): Executes the command.
+ */
+
 #ifndef COMMAND_H
 #define COMMAND_H
 //COMMAND AND MACRO-COMMAND
